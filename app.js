7135ac26ddae6e87e9ff40962589b4034d9a134f
Jaani <jaani@kooders.fi>
2016-12-17 19:52:39 +0200

    Changed API polling logic + various other improvements
    No longer does stupid things
    Updates DB only when necessary
    Only emits playerdata to client when something has really changed

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
@@ -8,13 +7,16 @@ var firebase = require('firebase')
 var admin = require("firebase-admin");
 admin.initializeApp({
     credential: admin.credential.cert({
+        "project_id": "scoretracker-9a7e5",
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDthT1+5DAEa0x6\nJ3OtPZoQ3ehDf55HI5dxnr6Iv2eLPeT9jV0lqY91SNzoo5rFkUd8tgpJdiQGQ2/B\n9ULAjtJ0a6FoERiP8d8uoL6HC3dTF3N+GlVlT1hXvnCaxXBvdPaKzPI1kIzJY1I6\nT7oZzqtzHY9g8aKMnlvqm1KQrCkChln86KgHrrcmwKHgg6A4BEEbS/MQ0+PQi0rt\nH7lDnIP1rYkUaTRjC3aW5yPs6kcN/ZzfGD72Z2+j/uLhwLpCKrrBc8XVJs8c/8Kh\ntvU52OgQGlhg/4lVfjmybXEUCdV/1FpOovE/jyopzeGkNE2X8ffsfV5iyTjzZ9Hw\n7ErCex2fAgMBAAECggEAMlY3dyni5zzaI1UzFbblbMeqjouAPrFTShgLZy2xQvgm\nDHomEHJD3eZIMuFhTpeAX/Swb2sjLVSjXaads/NQtK2OsmBVJURsORSF7FbIvgpN\nRp9ME8FiBo5sjNBlCKnwCfBvMFznCVMMPSFiXk/HVAkitrfe7BAviKPMNq7Vrhvk\nd0QNR4VzIbMyLpErPwHRGgAgB28A+dsMNUNRvEI5IKTkwT6/3+bR/hrqMY4p7Ubz\nLv4rAY18Ksj/3uQKbJKJAGQqGQYIwIHOydSQTrSFjSw3hSuhWrZbTyV8XPh/zobc\nQBYR4lXG3Z9K49cuY5rsaIKxwuwSYI3wx84UmXkdoQKBgQD38VQuUAwgAB8ixRZw\nmp9l6qrGMj2VLJpvRUfDkod+secEQCYICV742yZhK/wLzwP1Abn9mXw/u22d/D7y\ngZupOwMVXyKuHwmoitsCK1kgzuzZiRwxQx1eLv/TCDlgEPuOMCjjgi+MLSKz/nhS\nCtzBarj1fLKq/Hyal7ry4KSfTwKBgQD1PTUZWFQ5fvNDK85TRvdCJ1X0T20kLQ4S\nQ3Q5mjo1fCPCHM0F2kKQs39JChk0o6b6+Zlr7WDl6wQM9TZoGRZ6CP1tCKhS7rB/\nby2ZmhYyKjv9E3BIYUWiG3nwWrk9GtJ6tN/yDDHBRtEs46VFNW8DPWuGvzyhFdK7\nhyVa6cmIsQKBgB4EK0rICg+9tAdDkOnk7jMqa64+2tG6ap/Z5Uw56wldhDgxg+kp\nKUQ7U7Qj3QmY8EOzB885y57zk9uyc/Rr2GpuaHsn30zxGPw8gzMKCGo+YtIbFTyp\ngV6rlfD4Z+nHZIzcK7cPRa5UsITV478a7YrE3stpYz/r/THB+LH6nDNlAoGAMyY5\n/fhLUNamnDr1xK7HgXe82MD8LZBsH+kLw7vkKiWjO6hQJslYGuAlSzGdAjqj6DLJ\nChavoCS9aop2d43L/1YCrYwht3JrQ5kHtMLLoFjovCciwyXupRn/+96gRhtjDQ7O\nqqhadEp19Fviq6WyasWTuL4IQjzQACb25pheXXECgYEA4MYSEIeGLV+PFSc0272V\nbeS4BtMZN1jC0p70wQm3MmqezU+w4XSDZMB5f5pQXsfbhgcu4s1KQE+POdeNHM4a\ninfQ9YMaHUH2J9fbm93whwLcxEsHfj/zxGtA1G0GMS8NIFtrvzWdRl6VG/JPxLRW\nxn8WwZ1uWn9wS40omujmwsc=\n-----END PRIVATE KEY-----\n",
+        "client_email": "firebase-adminsdk-3j1si@scoretracker-9a7e5.iam.gserviceaccount.com",
     }),
     databaseURL: "https://scoretracker-9a7e5.firebaseio.com/"
 });
 
 // Initialize Firebase
 var config = {
-    apiKey: "",
+    apiKey: "AIzaSyCmBhbJCioIU0_9AWsrvyhEcZBHgH6S1gI",
     authDomain: "scoretracker-9a7e5.firebaseapp.com",
     databaseURL: "https://scoretracker-9a7e5.firebaseio.com",
     storageBucket: "scoretracker-9a7e5.appspot.com",
@@ -33,7 +35,7 @@ var args = require("minimist")(process.argv.slice(2))
 
 const PORT = process.env.PORT || 3000
 const HOST = process.env.HOST || '127.0.0.1'
-const POLLING_INTERVAL = 10000 //ms
+const POLLING_INTERVAL = 30000 //ms
 
 // Serve index.html
 app.use(function(req, res, next) {
@@ -47,35 +49,7 @@ app.use(function(req, res, next) {
 	})
 })
 
-var dbPlayers
 io.on("connection", function(socket){
-    // Creates change listeners for each player in our database
-    /*
-    database.ref("/players/").once('value').then(function(snapshot){
-        return;
-        var players = snapshot.val()
-        // Loop through all players
-        Object.keys(players).forEach(function(player) {
-             var player = players[player] // Individual player
-             var newListener = database.ref("players/"+player.playerName)
-             console.log("Creating a listener for: " + player.playerName)
-             newListener.on('value', function(snapshot){
-                // Broadcast individual player info to client
-                console.log("Something changed! Broadcasting..")
-                socket.emit("broadcastPlayer", {player: snapshot.val()})
-             })
-             // Add this listener to list
-             dbChangeListeners.push(newListener)
-        })
-    })
-    // Broadcasts all initial players to connectée
-    /*
-    database.ref("/players/").once('value').then(function(snapshot){
-        var players = snapshot.val()
-        socket.emit("broadcastPlayers", {players: players})
-    })
-    */
-
     socket.on("sign_in", function(data){
         var idToken = data.user
         var user = verifyUser(idToken).then(function(user){
@@ -83,11 +57,16 @@ io.on("connection", function(socket){
             // Fetch user's playerlist
             database.ref("/users/"+user.uid+"/playerlist").once('value').then(function(snapshot){
                 var playerlist = snapshot.val()
+                // Loop through all players 
                 Object.keys(playerlist).forEach(function(player) {
                     var player = playerlist[player] // Individual player
-                    database.ref("/players/"+player.battleNet).once('value').then(function(snapshot){
+                    console.log(player)
+                    var players = database.ref('players/'+player.battleNet);
+                    // Listen for changes for each player in user's playerlist
+                    players.on('value', function(snapshot) {
+                        // Something changed, emit new data
                         socket.emit("playerdata", {player: snapshot.val()})
-                    })
+                    });
                 })
             })
         }).catch(function(error){
@@ -199,49 +178,46 @@ function playerlistHasBattleNet(playerlist, battleNet){
     return has
 }
 
-// Starts an interval which polls the UNOFFICIAL OW -api
-var polling = setInterval(function(){
-    return;
-    // Get all current players in our database
-    database.ref("/players/").once('value').then(function(snapshot){
-        var players = snapshot.val()
+// Updates database with new data from OW Unofficial Api
+database.ref("/players/").once('value').then(function(snapshot){
+    var players = snapshot.val()
+    setInterval(function(){
         // Loop through all players
         Object.keys(players).forEach(function(player) {
-            var player = players[player] // Individual player
-
-            // GET individual player data from API
-            axios.get("https://api.lootbox.eu/pc/eu/"+player.playerName+"/profile")
+            // Get data for that player from API
+            axios.get("https://api.lootbox.eu/pc/eu/"+player+"/profile")
             .then(function(response){
-                var currentPlayerData = response.data.data
-                var postData = {
-                    avatar: currentPlayerData.avatar,
-                    
-                };
-
-                
+                var player_current = response.data.data
                 var timestamp = Math.floor(Date.now() / 1000)
-                var newRank = currentPlayerData.competitive.rank
-                // Update rank
-                database.ref("/players/"+player.playerName).once('value').then(function(snapshot){
-                    var player = snapshot.val()
-                    var newest = player.ranks[Object.keys(player.ranks)[Object.keys(player.ranks).length-1]]
+                var newRank = player_current.competitive.rank
+                
+                // Get (perhaps outdated) player info from database
+                database.ref("/players/"+player).once('value').then(function(snapshot){
+                    var player_db = snapshot.val()
+                    var newestRankEntry = player_db.ranks[Object.keys(player_db.ranks)[Object.keys(player_db.ranks).length-1]]
 
-                    // Update only if rank has changed
-                    if (newest.rank != newRank){
-                        var newRankRef = database.ref("/players/"+player.playerName+"/ranks").push();
+                    // Only update if new data and current database data are different
+                    if (newestRankEntry.rank != newRank && newestRankEntry.timestamp != timestamp){
+                        var newRankRef = database.ref("/players/"+player+"/ranks").push();
                         newRankRef.set({
+                            rank_img: player_current.competitive.rank_img,
                             rank: newRank,
                             timestamp: timestamp
                         });
                     }
-                })
 
-                // Update other info
-                database.ref("players/"+player.playerName).update(postData)
-            })
-        });
-    })
-}, POLLING_INTERVAL)
+                    // Update avatar and maybe other info (if has changed)
+                    if (player_current.avatar != player_db.avatar){
+                        var updatedInfo = {
+                            avatar: player_current.avatar
+                        }
+                        database.ref("players/"+player).update(updatedInfo)
+                    }
+                })
+            })   
+        })
+    }, POLLING_INTERVAL)
+})
 
 server.listen(PORT, function () {
   console.log("Up and running at port :: " + PORT)
